7626b56f481146821f0112bd3389c0362187c728
MohammadNemer <mohammadnemer1996@hotmail.com>
2019-10-23 13:45:03 +0300

    test

--- src/main/java/com/example/demo/SchoolDemoApplication.java
+++ src/main/java/com/example/demo/SchoolDemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SchoolDemoApplication {
 
 	public static void main(String[] args) {
-		
+		SpringApplication.run(SchoolDemoApplication.class, args);
 	}
 
 }
